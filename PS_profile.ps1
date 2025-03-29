9f3d23c5108760ef89149381741bc3d54ff454f4
Wilhen Wilgred Figueredo Hernandez <wfigueredoh@Falabella.com>
2025-03-29 15:29:50 -0500

    dotfiles windows

---
+++ PS_profile.ps1
@@ -0,0 +1,12 @@
+Import-Module -Name Terminal-Icons
+#"C:\Users\wfigueredoh\OneDrive - Falabella\Documents\BK_WT\oh-my-posh\themes\WF.omp.json"
+
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
+
+#oh-my-posh init pwsh --config "C:\Users\wfigueredoh\OneDrive - Falabella\Documents\BK_WT\oh-my-posh\themes\WF.omp.json" | Invoke-Expression
+
+#oh-my-posh init pwsh --config "C:\Users\wfigueredoh\OneDrive - Falabella\Documents\BK_WT\oh-my-posh\themes\jlab_2021.omp.json" | Invoke-Expression
+
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jblab_2021.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/WF.omp.json" | Invoke-Expression
+
