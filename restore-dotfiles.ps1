7b75e456b1ebd93763216d5f844dbd723e4e82d2
Wilhen Figueredo <wilhen199@gmail.com>
2025-03-31 21:31:09 -0500

    🔄 Sincronización automática 2025-03-31 21:31

--- restore-dotfiles.ps1
+++ restore-dotfiles.ps1
@@ -5,8 +5,8 @@ $branch = "windows"
 
 # Ubicaciones de los dotfiles
 $dotfiles = @{
-    "$PROFILE"                                                                                    = "PS_profile.ps1"
-    "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = "WT_settings.json"
+    "PS_profile.ps1"   = "$PROFILE"
+    "WT_settings.json" = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 }
 
 # Clonar o actualizar el repo
