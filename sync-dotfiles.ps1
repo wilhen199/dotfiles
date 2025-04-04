6f806c59bf1bf04f705ad80a1e4c10bc9a4df51a
Wilhen Figueredo <wilhen199@gmail.com>
2025-04-04 14:01:09 -0500

    🔄 Sincronización automática 2025-04-04 14:01

--- sync-dotfiles.ps1
+++ sync-dotfiles.ps1
@@ -7,6 +7,7 @@ $branch = "windows"
 $dotfiles = @{
     "$PROFILE"                                                                                    = "PS_profile.ps1"
     "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = "WT_settings.json"
+    "$env:POSH_THEMES_PATH/WF.omp.json" = "WF.omp.json"
 }
 
 # Clonar o actualizar el repo
