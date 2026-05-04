aaa91b68c518025e4fa12cf9b89de278bc99dba6
Wilhen Wilgred Figueredo Hernandez <wfigueredoh@Falabella.com>
2026-05-04 18:25:13 -0500

    🔄 Sincronización automática 2026-05-04 18:25

--- PS_profile.ps1
+++ PS_profile.ps1
@@ -1,5 +1,7 @@
-Import-Module -Name Terminal-Icons
-#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
-#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jblab_2021.omp.json" | Invoke-Expression
+# Oh My Posh primero
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/WF.omp.json" | Invoke-Expression
 
+# Lazy load
+function Load-TerminalIcons {
+    Import-Module Terminal-Icons
+}
